265d3dd34fb0d97309fbb5fbd9f6df825fcddb2c
Judah Lamm <Judah.Lamm@gmail.com>
2015-07-07 09:58:59 -1000

    generates an array of prime numbers to check

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -7,5 +7,34 @@
 module.exports = function(numbers){
   // do work here
 
+  var finalNumber = 2;
+  var arr = [];
+
+  function primeChecker(num) {
+      if(num < 2){
+          finalNumber = 2;
+      }
+
+      for (var i = 2; i < num; i++) {
+          if(num%i===0){
+              return false;
+          }else{
+              return true;
+          }
+      }
+  }
+
+  function primeStart(num){
+      for(var i = 7; i <= num; i++){
+          if(primeChecker(i)){
+              arr.push(i)
+          }
+      }
+      console.log(arr);
+  }
+
+
+  primeStart(19);
+
   return 0;
 };
